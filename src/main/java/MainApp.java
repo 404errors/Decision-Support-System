1eec357e9dcafa946e76c2279ae9d2a88112fa5a
phucnn <nguoiviet95@gmail.com>
2017-12-04 12:45:18 +0700

    - update program icon

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -21,6 +22,7 @@ public class MainApp extends Application {
         stage.show();
         stage.setResizable(false);
         stage.sizeToScene();
+        stage.getIcons().add(new Image("icons/dss.png"));
     }
 
 }
